fc3332b1a63d6d5356d366508ea1b49b97b89aed
Sergio <sergio.majluf@gmail.com>
2019-01-02 00:03:43 -0300

    Primer commit

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const app = express();
+
+const hbs = require('hbs');
+require('./hbs/helpers/helpers');
+
+const port = process.env.PORT || 3000;
+
+app.use(express.static(__dirname + '/public'));
+
+// Express HBS Engine
+hbs.registerPartials(__dirname + '/views/parciales/');
+app.set('view engine', 'hbs');
+
+
+app.get('/', (req, res) => {
+
+    res.render('home', {
+        nombre: 'SERGIO majluF'
+    });
+});
+
+app.get('/about', (req, res) => {
+
+    res.render('about', {
+        nombre: 'acerca de esta página'
+    });
+});
+
+
+app.listen(port, () => {
+    console.log(`Escuchando puerto ${ port }`);
+})
